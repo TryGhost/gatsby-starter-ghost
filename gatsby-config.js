6cdc9960fcce9997f5380614c53b31de374b43ab
Aileen Nowak <AileenCGN@gmail.com>
2018-12-11 08:56:01 +0000

    ➖ Removed Remark dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,28 +14,6 @@ module.exports = {
         name: 'pages',
       },
     },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
-        ],
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
