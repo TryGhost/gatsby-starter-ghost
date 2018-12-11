7920d7a450c24d69e6eb6a8fee17b7d63d6ed9da
Aileen Nowak <AileenCGN@gmail.com>
2018-12-11 13:46:13 +0000

    ➖ Removed typography dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,47 +1,41 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Ghost Gatsby Starter',
-    author: 'Ghost',
-    description: 'Thoughts, stories and ideas',
-    siteUrl: 'https://tryghost.github.io/gatsby-starter-ghost/',
-  },
-  pathPrefix: '/gatsby-starter-ghost',
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
+    siteMetadata: {
+        title: `Ghost Gatsby Starter`,
+        author: `Ghost`,
+        description: `Thoughts, stories and ideas`,
+        siteUrl: `https://tryghost.github.io/gatsby-starter-ghost/`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Ghost`,
-        short_name: `TryGhost`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/assets/gatsby-icon.png`,
-      },
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: 'gatsby-plugin-typography',
-      options: {
-        pathToConfigModule: 'src/utils/typography',
-      },
-    },
-  ],
+    pathPrefix: `/gatsby-starter-ghost`,
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/pages`,
+                name: `pages`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                //trackingId: `ADD YOUR TRACKING ID HERE`,
+            },
+        },
+        `gatsby-plugin-feed`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Gatsby Starter Ghost`,
+                short_name: `TryGhost`,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/assets/gatsby-icon.png`,
+            },
+        },
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-react-helmet`,
+    ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,46 +5,41 @@ import Helmet from 'react-helmet'
 
 import Bio from '../components/Bio'
 import Layout from '../components/Layout'
-import { rhythm } from '../utils/typography'
 
 class BlogIndex extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const siteDescription = get(
-      this,
-      'props.data.site.siteMetadata.description'
-    )
-    const posts = get(this, 'props.data.allMarkdownRemark.edges')
+    render() {
+        const siteTitle = get(this, `props.data.site.siteMetadata.title`)
+        const siteDescription = get(
+            this,
+            `props.data.site.siteMetadata.description`
+        )
+        const posts = get(this, `props.data.allMarkdownRemark.edges`)
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <Helmet
-          htmlAttributes={{ lang: 'en' }}
-          meta={[{ name: 'description', content: siteDescription }]}
-          title={siteTitle}
-        />
-        <Bio />
-        {posts.map(({ node }) => {
-          const title = get(node, 'frontmatter.title') || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-            </div>
-          )
-        })}
-      </Layout>
-    )
-  }
+        return (
+            <Layout location={this.props.location} title={siteTitle}>
+                <Helmet
+                    htmlAttributes={{ lang: `en` }}
+                    meta={[{ name: `description`, content: siteDescription }]}
+                    title={siteTitle}
+                />
+                <Bio />
+                {posts.map(({ node }) => {
+                    const title = get(node, `frontmatter.title`) || node.fields.slug
+                    return (
+                        <div key={node.fields.slug}>
+                            <h3>
+                                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                                    {title}
+                                </Link>
+                            </h3>
+                            <small>{node.frontmatter.date}</small>
+                            <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+                        </div>
+                    )
+                })}
+            </Layout>
+        )
+    }
 }
 
 export default BlogIndex
