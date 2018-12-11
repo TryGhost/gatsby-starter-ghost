e2650ce79997495451418024bed6eee165d5e8a5
Aileen Nowak <AileenCGN@gmail.com>
2018-12-11 14:52:10 +0000

    🚨 Fixed linting issues in siteConfig

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,21 +1,21 @@
 module.exports = {
-  siteTitle: "Ghost",
-  siteTitleAlt: "Ghost Gatsby Starter", // This allows an alternative site title for SEO schema.
-  publisher: "Publisher Name", // Organization name used for SEO schema
-  siteDescription:
-    "A starter template to build amazing static websites with Ghost and Gatbsy",
-  siteUrl: "https://tryghost.github.io/gatsby-starter-ghost", // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
-  postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
-  postsPerPage: 6, // Number of posts shown on paginated pages
-  author: "Ghost User", // Author for RSS author segment and SEO schema
-  authorUrl: "https://twitter.com/TryGhost", // URL used for author and publisher schema, can be a social profile or other personal site
-  userTwitter: "@TryGhost", // Change for Twitter Cards
-  shortTitle: "Ghost", // Used for App manifest e.g. Mobile Home Screen
-  shareImage: "/logos/share.jpg", // Open Graph Default Share Image. 1200x1200 is recommended
-  shareImageWidth: 900, // Change to the width of your default share image
-  shareImageHeight: 600, // Change to the height of your default share image
-  siteLogo: "/logos/logo-512.png", // Logo used for SEO, RSS, and App manifest
-  backgroundColor: "#e9e9e9", // Used for Offline Manifest
-  themeColor: "#121212", // Used for Offline Manifest
-  copyright: "Copyright © 2018 Ghost User" // Copyright string for the RSS feed
-};
+    siteTitle: `Ghost`,
+    siteTitleAlt: `Ghost Gatsby Starter`, // This allows an alternative site title for SEO schema.
+    publisher: `Publisher Name`, // Organization name used for SEO schema
+    siteDescription:
+    `A starter template to build amazing static websites with Ghost and Gatbsy`,
+    siteUrl: `https://tryghost.github.io/gatsby-starter-ghost`, // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+    postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
+    postsPerPage: 6, // Number of posts shown on paginated pages
+    author: `Ghost User`, // Author for RSS author segment and SEO schema
+    authorUrl: `https://twitter.com/TryGhost`, // URL used for author and publisher schema, can be a social profile or other personal site
+    userTwitter: `@TryGhost`, // Change for Twitter Cards
+    shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
+    shareImage: `/logos/share.jpg`, // Open Graph Default Share Image. 1200x1200 is recommended
+    shareImageWidth: 900, // Change to the width of your default share image
+    shareImageHeight: 600, // Change to the height of your default share image
+    siteLogo: `/logos/logo-512.png`, // Logo used for SEO, RSS, and App manifest
+    backgroundColor: `#e9e9e9`, // Used for Offline Manifest
+    themeColor: `#121212`, // Used for Offline Manifest
+    copyright: `Copyright © 2018 Ghost User`, // Copyright string for the RSS feed
+}
