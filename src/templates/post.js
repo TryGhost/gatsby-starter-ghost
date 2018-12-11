70ba3dbe09fedec023481a668fa3d90c80f499cc
Aileen Nowak <AileenCGN@gmail.com>
2018-12-11 14:51:09 +0000

    🎨 Post template improvements

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,56 +3,56 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Prism from 'prismjs'
 
-import { Layout } from '../../components/common/layout'
-import { MetaData } from '../../components/common/meta'
+import { Layout } from '../components/common/layout'
+// import { MetaData } from '../components/common/meta'
 
 class Post extends React.Component {
-  componentDidMount() {
+    componentDidMount() {
     // TODO: Prism for Webpack currently supports basic languages. `handlebars`,
     // `yaml`, and `json` are not amongst those. To load those languages, we'd
     // need to load them specifically following the webpack instructions here:
     // https://prismjs.com/#examples and https://github.com/mAAdhaTTah/babel-plugin-prismjs
     // The other option is to create a plugin for GhostPosts.
-    Prism.highlightAll()
-  }
+        Prism.highlightAll()
+    }
 
-  render() {
-    const post = this.props.data.ghostPost
-    const image = post.feature_image || ``
+    render() {
+        const post = this.props.data.ghostPost
+        const image = post.feature_image || ``
 
-    return (
+        return (
       <>
-        <MetaData
-          data={this.props.data}
-          location={this.props.location}
-          type="article"
-          image={image}
-        />
+        {/* <MetaData
+            data={this.props.data}
+            location={this.props.location}
+            type="article"
+            image={image}
+        /> */}
         <Layout>
-          <div>
-            <article>
-              <h1>{post.title}</h1>
-              <section
-                className="post-content external-scripts"
-                dangerouslySetInnerHTML={{ __html: post.html }}
-              />
-            </article>
-          </div>
+            <div>
+                <article>
+                    <h1>{post.title}</h1>
+                    <section
+                        className="post-content external-scripts"
+                        dangerouslySetInnerHTML={{ __html: post.html }}
+                    />
+                </article>
+            </div>
         </Layout>
       </>
-    )
-  }
+        )
+    }
 }
 
 Post.propTypes = {
-  data: PropTypes.shape({
-    ghostPost: PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      html: PropTypes.string.isRequired,
-      feature_image: PropTypes.string,
+    data: PropTypes.shape({
+        ghostPost: PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            html: PropTypes.string.isRequired,
+            feature_image: PropTypes.string,
+        }).isRequired,
     }).isRequired,
-  }).isRequired,
-  location: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
 }
 
 export default Post
