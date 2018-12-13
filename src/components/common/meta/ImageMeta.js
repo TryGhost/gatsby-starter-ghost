692f3f95a5ff89153505072430af0b7109c6a9dd
Aileen Nowak <AileenCGN@gmail.com>
2018-12-13 17:41:09 +0000

    🔧 Use siteConfig

--- src/components/common/meta/ImageMeta.js
+++ src/components/common/meta/ImageMeta.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import Helmet from "react-helmet"
 import PropTypes from 'prop-types'
+import config from '../../../utils/siteConfig'
 
 const ImageMeta = ({ image }) => (
     <Helmet>
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={image} />
         <meta property="og:image" content={image} />
-        {/* TODO: set those via config */}
-        <meta property="og:image:width" content="1000" />
-        <meta property="og:image:height" content="523" />
+        <meta property="og:image:width" content={config.shareImageWidth} />
+        <meta property="og:image:height" content={config.shareImageHeight} />
     </Helmet >
 )
 
--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,21 +1,22 @@
 module.exports = {
     siteTitle: `Ghost`,
-    siteTitleAlt: `Ghost Gatsby Starter`, // This allows an alternative site title for SEO schema.
-    publisher: `Publisher Name`, // Organization name used for SEO schema
     siteDescription:
     `A starter template to build amazing static websites with Ghost and Gatbsy`,
-    siteUrl: `https://tryghost.github.io/gatsby-starter-ghost`, // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+    siteUrl: `https://tryghost.github.io/`, // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+
     postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
     postsPerPage: 6, // Number of posts shown on paginated pages
-    author: `Ghost User`, // Author for RSS author segment and SEO schema
-    authorUrl: `https://twitter.com/TryGhost`, // URL used for author and publisher schema, can be a social profile or other personal site
-    userTwitter: `@TryGhost`, // Change for Twitter Cards
+
+    siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for SEO schema.
+    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatbsy`,
+    twitterUrl: `https://twitter.com/tryghost`, // URL of your publication's Twitter profile
+    facebookUrl: `https://www.facebook.com/ghost`, // URL of your publication's Facebook Page
+    shareImage: `/images/Ghost-Docs.jpg`, // fallback image for meta data. Will be used, when no post/tag/author image specified. 1200x1200 is recommended
+    shareImageWidth: 1000, // Change to the width of your default share image
+    shareImageHeight: 523, // Change to the height of your default share image
+
     shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
-    shareImage: `/logos/share.jpg`, // Open Graph Default Share Image. 1200x1200 is recommended
-    shareImageWidth: 900, // Change to the width of your default share image
-    shareImageHeight: 600, // Change to the height of your default share image
-    siteLogo: `/logos/logo-512.png`, // Logo used for SEO, RSS, and App manifest
+    siteIcon: `/favicon.ico`, // Logo used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#121212`, // Used for Offline Manifest
-    copyright: `Copyright © 2018 Ghost User`, // Copyright string for the RSS feed
 }
