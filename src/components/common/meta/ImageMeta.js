600b69c9e842957e9aa16aaab7714918e93d52a3
Aileen Nowak <AileenCGN@gmail.com>
2018-12-14 17:21:16 +0000

    🎨 Allow empty image to be passed to ImageMeta

--- src/components/common/meta/ImageMeta.js
+++ src/components/common/meta/ImageMeta.js
@@ -3,18 +3,24 @@ import Helmet from "react-helmet"
 import PropTypes from 'prop-types'
 import config from '../../../utils/siteConfig'
 
-const ImageMeta = ({ image }) => (
-    <Helmet>
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content={image} />
-        <meta property="og:image" content={image} />
-        <meta property="og:image:width" content={config.shareImageWidth} />
-        <meta property="og:image:height" content={config.shareImageHeight} />
-    </Helmet >
-)
+const ImageMeta = ({ image }) => {
+    if (!image) {
+        return null
+    }
+
+    return (
+        <Helmet>
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:image" content={image} />
+            <meta property="og:image" content={image} />
+            <meta property="og:image:width" content={config.shareImageWidth} />
+            <meta property="og:image:height" content={config.shareImageHeight} />
+        </Helmet >
+    )
+}
 
 ImageMeta.propTypes = {
-    image: PropTypes.string.isRequired,
+    image: PropTypes.string,
 }
 
 export default ImageMeta
