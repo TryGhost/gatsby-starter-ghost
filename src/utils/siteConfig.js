d01b9f94923dd063976c62864215504a7dc9f4c9
Aileen Nowak <AileenCGN@gmail.com>
2018-12-14 16:07:34 +0000

    📝 Small comment changes in siteConfig

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -4,13 +4,14 @@ module.exports = {
     `A starter template to build amazing static websites with Ghost and Gatbsy`,
     siteUrl: `http://localhost:8000`, // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
 
+    // TODO: work with those properties
     postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
     postsPerPage: 6, // Number of posts shown on paginated pages
 
     siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for SEO schema.
     siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatbsy`,
-    publisherName: `Ghost`,
-    twitterUser: `@Tryghost`, // Username of your publication's Twitter profile
+    publisherName: `Ghost`, // Name used to reference the publisher for meta data
+    twitterUser: `@tryghost`, // Username of your publication's Twitter profile
     facebookUrl: `https://www.facebook.com/ghost`, // URL of your publication's Facebook Page
     shareImage: `/images/Ghost-Docs.jpg`, // fallback image for meta data. Will be used, when no post/tag/author image specified. 1200x1200 is recommended
     shareImageWidth: 1000, // Change to the width of your default share image
