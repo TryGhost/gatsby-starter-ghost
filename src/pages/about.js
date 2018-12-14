6ba1254c1f45a589382bd11c38f8372125b51999
Aileen Nowak <AileenCGN@gmail.com>
2018-12-14 17:40:18 +0000

    🐛 Fix Layout reference in About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Layout } from '../components/common/layout'
+import { Layout } from '../components/common'
 import { MetaData } from '../components/common/meta'
 
 const AboutPage = ({ location }) => (
