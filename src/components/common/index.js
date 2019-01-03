5918683ba417c9b312f7dbbc99102098e95e6cce
Rish <zrishabhgarg@gmail.com>
2019-01-03 16:31:35 +0530

    🔥 Deleted common Tags helper as its used from Ghost-SDK

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,4 +1,3 @@
 export { default as Layout } from './Layout'
 export { default as PostCard } from './PostCard'
-export { default as Tags } from './Tags'
 export { default as Pagination } from './Pagination'
