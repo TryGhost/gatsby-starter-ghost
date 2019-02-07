31c9b56c4c6b9f8e73553e36ee6822dbc32299cf
Aileen Nowak <AileenCGN@gmail.com>
2019-02-07 12:29:45 +0700

    Updated post slugs in PostCard component to start with `/`
    no issue
    
    - With the latest Gatsby update, the `Link` component was complaining about passing external URLs to it
    - This was identified to happen in our `PostCard` component and due to our post slugs (which **are** internal links and the usage of `Link` is therefore correct)
    - Adding a `/` prefix before the post slug to be passed to `Link` solved the issue

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -5,7 +5,7 @@ import { Tags } from '@tryghost/helpers-gatsby'
 import { readingTime as readingTimeHelper } from '@tryghost/helpers'
 
 const PostCard = ({ post }) => {
-    const url = `${post.slug}/`
+    const url = `/${post.slug}/`
     const readingTime = readingTimeHelper(post)
 
     return (
