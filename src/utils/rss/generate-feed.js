70618187724c1697722af7bae91fc4ad8f0d5e77
Aileen Nowak <AileenCGN@gmail.com>
2019-02-12 14:06:31 +0700

    🎨 Use favicon as set in siteConfig for RSS feed
    no issue
    
    - Use the set `siteIcon` from our `siteConfig` instead of hardcoding the favicon

--- src/utils/rss/generate-feed.js
+++ src/utils/rss/generate-feed.js
@@ -57,7 +57,7 @@ const generateRSSFeed = function generateRSSFeed(siteConfig) {
                 generator: `Ghost 2.9`,
                 feed_url: `${siteConfig.siteUrl}/rss/`,
                 site_url: `${siteConfig.siteUrl}/`,
-                image_url: `${siteConfig.siteUrl}/favicon.png`,
+                image_url: `${siteConfig.siteUrl}/${siteConfig.siteIcon}`,
                 ttl: `60`,
                 custom_namespaces: {
                     content: `http://purl.org/rss/1.0/modules/content/`,
