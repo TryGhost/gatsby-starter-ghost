6ab876067f9e3932904e5ffcce6ed5068ab4d6d8
Aileen Nowak <AileenCGN@gmail.com>
2019-02-26 13:12:20 +0700

    ➕ Added gatsby advanced sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -108,7 +108,86 @@ module.exports = {
                 ],
             },
         },
-        `gatsby-plugin-ghost-sitemap`,
+        {
+            resolve: `gatsby-plugin-advanced-sitemap`,
+            options: {
+                query: `
+                {
+                    allGhostPost(sort: {order: ASC, fields: published_at}) {
+                        edges {
+                            node {
+                                id
+                                slug
+                                updated_at
+                                created_at
+                                feature_image
+                            }
+                        }
+                    }
+                    allGhostPage(sort: {order: ASC, fields: published_at}) {
+                        edges {
+                            node {
+                                id
+                                slug
+                                updated_at
+                                created_at
+                                feature_image
+                            }
+                        }
+                    }
+                    allGhostTag(sort: {order: ASC, fields: name}) {
+                        edges {
+                            node {
+                                id
+                                slug
+                                feature_image
+                            }
+                        }
+                    }
+                    allGhostAuthor(sort: {order: ASC, fields: name}) {
+                        edges {
+                            node {
+                                id
+                                slug
+                                profile_image
+                            }
+                        }
+                    }
+                }`,
+                mapping: {
+                    allGhostPost: {
+                        name: `posts`,
+                        path: `/`,
+                        source: `posts`,
+                    },
+                    allGhostTag: {
+                        name: `tags`,
+                        path: `tag`,
+                        source: `tags`,
+                    },
+                    allGhostAuthor: {
+                        name: `authors`,
+                        path: `author`,
+                        source: `authors`,
+                    },
+                    allGhostPage: {
+                        name: `pages`,
+                        path: `/`,
+                        source: `pages`,
+                    },
+                },
+                exclude: [
+                    `/dev-404-page`,
+                    `/404`,
+                    `/404.html`,
+                    `/offline-plugin-app-shell-fallback`,
+                    `/data-schema`,
+                    `/data-schema-author`,
+                    `/data-schema-page`,
+                ],
+                createLinkInHead: true,
+            },
+        },
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-force-trailing-slashes`,
         `gatsby-plugin-offline`,
