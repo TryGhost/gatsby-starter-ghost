0633f7bd9e27622de3d20d87affa238fb84fc081
Zion Dials <me@ziondials.com>
2019-08-25 13:20:48 -0400

    Added gatsby-plugin-catch-links

--- gatsby-config.js
+++ gatsby-config.js
@@ -179,6 +179,7 @@ module.exports = {
                 addUncaughtPages: true,
             },
         },
+        `gatsby-plugin-catch-links`,
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-force-trailing-slashes`,
         `gatsby-plugin-offline`,
