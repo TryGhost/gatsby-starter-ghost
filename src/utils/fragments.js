4c7457ad3e7e1831a90b3c0a4a8c4f6f947aa840
Aileen Nowak <AileenCGN@gmail.com>
2019-11-18 14:55:29 +0700

    ⬆️  Updated gatsby-source-ghost@4.0.3 and added missing fields to fragments
    refs #90
    
    - the Content API fields `canonical_url`, `visibility`, and `reading_time` were missing in our default schema creation in [gatsby-source-ghost](https://github.com/TryGhost/gatsby-source-ghost) plugin. This has been fixed with `v4.0.3`
    - added those fields also to fragments to make them available in queries

--- src/utils/fragments.js
+++ src/utils/fragments.js
@@ -50,6 +50,7 @@ export const ghostPostFields = graphql`
         feature_image
         excerpt
         custom_excerpt
+        visibility
 
         # Dates formatted
         created_at_pretty: created_at(formatString: "DD MMMM, YYYY")
@@ -119,12 +120,14 @@ export const ghostPostFields = graphql`
 
         # Additional fields
         url
+        canonical_url
         uuid
         page
         codeinjection_foot
         codeinjection_head
         codeinjection_styles
         comment_id
+        reading_time
     }
 `
 
@@ -138,6 +141,7 @@ export const ghostPageFields = graphql`
         feature_image
         excerpt
         custom_excerpt
+        visibility
 
         # Dates formatted
         created_at_pretty: created_at(formatString: "DD MMMM, YYYY")
@@ -207,12 +211,14 @@ export const ghostPageFields = graphql`
 
         # Additional fields
         url
+        canonical_url
         uuid
         page
         codeinjection_foot
         codeinjection_head
         codeinjection_styles
         comment_id
+        reading_time
     }
 `
 
