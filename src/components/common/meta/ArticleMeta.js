413dbada8dec5abecbbdedcbd202b8d657308b65
Aileen Nowak <AileenCGN@gmail.com>
2020-06-01 18:19:15 +1200

    ⬆️  Updated dependencies

--- src/components/common/meta/ArticleMeta.js
+++ src/components/common/meta/ArticleMeta.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Helmet from "react-helmet"
+import { Helmet } from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 import PropTypes from 'prop-types'
 import _ from 'lodash'
