ff61e4bb70d1be8c77f52ef67914e71594b94fb0
Ryan Coleman <ryan@stackery.io>
2020-10-14 14:14:00 -0700

    uncomment stuff that was broken before rebase

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,22 +5,22 @@ const generateRSSFeed = require(`./src/utils/rss/generate-feed`)
 
 let ghostConfig
 
-// try {
-//     ghostConfig = require(`./.ghost`)
-// } catch (e) {
-//     ghostConfig = {
-//         production: {
-//             apiUrl: process.env.GHOST_API_URL,
-//             contentApiKey: process.env.GHOST_CONTENT_API_KEY,
-//         },
-//     }
-// } finally {
-//     const { apiUrl, contentApiKey } = process.env.NODE_ENV === `development` ? ghostConfig.development : ghostConfig.production
+try {
+    ghostConfig = require(`./.ghost`)
+} catch (e) {
+    ghostConfig = {
+        production: {
+            apiUrl: process.env.GHOST_API_URL,
+            contentApiKey: process.env.GHOST_CONTENT_API_KEY,
+        },
+    }
+} finally {
+    const { apiUrl, contentApiKey } = process.env.NODE_ENV === `development` ? ghostConfig.development : ghostConfig.production
 
-//     if (!apiUrl || !contentApiKey || contentApiKey.match(/<key>/)) {
-//         throw new Error(`GHOST_API_URL and GHOST_CONTENT_API_KEY are required to build. Check the README.`) // eslint-disable-line
-//     }
-// }
+    if (!apiUrl || !contentApiKey || contentApiKey.match(/<key>/)) {
+        throw new Error(`GHOST_API_URL and GHOST_CONTENT_API_KEY are required to build. Check the README.`) // eslint-disable-line
+    }
+}
 
 if (process.env.NODE_ENV === `production` && config.siteUrl === `http://localhost:8000` && !process.env.SITEURL) {
     throw new Error(`siteUrl can't be localhost and needs to be configured in siteConfig. Check the README.`) // eslint-disable-line
@@ -94,26 +94,26 @@ module.exports = {
               `,
             },
         },
-        // {
-        //     resolve: `gatsby-plugin-feed`,
-        //     options: {
-        //         query: `
-        //         {
-        //             allGhostSettings {
-        //                 edges {
-        //                     node {
-        //                         title
-        //                         description
-        //                     }
-        //                 }
-        //             }
-        //         }
-        //       `,
-        //         feeds: [
-        //             generateRSSFeed(config),
-        //         ],
-        //     },
-        // },
+        {
+            resolve: `gatsby-plugin-feed`,
+            options: {
+                query: `
+                {
+                    allGhostSettings {
+                        edges {
+                            node {
+                                title
+                                description
+                            }
+                        }
+                    }
+                }
+              `,
+                feeds: [
+                    generateRSSFeed(config),
+                ],
+            },
+        },
         {
             resolve: `gatsby-plugin-advanced-sitemap`,
             options: {
