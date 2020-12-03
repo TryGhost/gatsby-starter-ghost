99e837692850072627c89b323ba1ce9649332fc3
Yann hulot <yann.hulot@activepipe.com>
2020-12-03 19:25:47 +1000

    add a few things

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,15 +1,15 @@
 module.exports = {
-    siteUrl: `http://localhost:8000`, // Site domain. Do not include a trailing slash!
+    siteUrl: "https://blog.tesolhub.co", // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `TesolHub`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `This blog will help you find jobs and help you to start your TESOL career.`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `TesolHub`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
