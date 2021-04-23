4d802f9e116e51ebba6a31f8813feb36495f9a9c
Ryan Coleman <ryan@stackery.io>
2021-04-23 10:29:53 -0700

    demo iteration

--- src/incrementLike/index.js
+++ src/incrementLike/index.js
@@ -1,6 +1,21 @@
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = async (event, context) => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
 
-  return {};
+  const result = await ddb.update({
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      id: event.pathParameters.id
+    },
+    UpdateExpression: 'ADD likes :one',
+    ExpressionAttributeValues: {
+      ':one': 1
+    },
+    ReturnValues: 'UPDATED_NEW'
+  }).promise();
+
+  return result.Attributes
 };
