2dc834cb23203d759512e5084d387f7ef269e1e3
Rodney Matambo <rodney@gatsbyjs.com>
2021-09-03 10:47:53 -0400

    migrating from gatsby image to gatsby-plugin-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,6 +68,7 @@ module.exports = {
                 name: `images`,
             },
         },
+        `gatsby-plugin-image`,
         `gatsby-plugin-sharp`,
         `gatsby-transformer-sharp`,
         {
