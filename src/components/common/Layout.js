663e06d684abc875d40d5d3ee7aeef3d46124dce
Rodney Matambo <rodney@gatsbyjs.com>
2021-09-03 10:47:53 -0400

    migrating from gatsby image to gatsby-plugin-image

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
 import { Link, StaticQuery, graphql } from "gatsby";
-import Img from "gatsby-image";
+import { GatsbyImage } from "gatsby-plugin-image";
 
 import { Navigation } from ".";
 import config from "../../utils/siteConfig";
@@ -27,153 +27,145 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
         ? `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}`
         : null;
 
-    return (
-        <>
-            <Helmet>
-                <html lang={site.lang} />
-                <style type="text/css">{`${site.codeinjection_styles}`}</style>
-                <body className={bodyClass} />
-            </Helmet>
+    return <>
+        <Helmet>
+            <html lang={site.lang} />
+            <style type="text/css">{`${site.codeinjection_styles}`}</style>
+            <body className={bodyClass} />
+        </Helmet>
 
-            <div className="viewport">
-                <div className="viewport-top">
-                    {/* The main header section on top of the screen */}
-                    <header
-                        className="site-head"
-                        style={{
-                            ...(site.cover_image && {
-                                backgroundImage: `url(${site.cover_image})`,
-                            }),
-                        }}
-                    >
-                        <div className="container">
-                            <div className="site-mast">
-                                <div className="site-mast-left">
-                                    <Link to="/">
-                                        {site.logo ? (
-                                            <img
-                                                className="site-logo"
-                                                src={site.logo}
-                                                alt={site.title}
-                                            />
-                                        ) : (
-                                            <Img
-                                                fixed={
-                                                    data.file.childImageSharp
-                                                        .fixed
-                                                }
-                                                alt={site.title}
-                                            />
-                                        )}
-                                    </Link>
-                                </div>
-                                <div className="site-mast-right">
-                                    {site.twitter && (
-                                        <a
-                                            href={twitterUrl}
-                                            className="site-nav-item"
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                        >
-                                            <img
-                                                className="site-nav-icon"
-                                                src="/images/icons/twitter.svg"
-                                                alt="Twitter"
-                                            />
-                                        </a>
+        <div className="viewport">
+            <div className="viewport-top">
+                {/* The main header section on top of the screen */}
+                <header
+                    className="site-head"
+                    style={{
+                        ...(site.cover_image && {
+                            backgroundImage: `url(${site.cover_image})`,
+                        }),
+                    }}
+                >
+                    <div className="container">
+                        <div className="site-mast">
+                            <div className="site-mast-left">
+                                <Link to="/">
+                                    {site.logo ? (
+                                        <img
+                                            className="site-logo"
+                                            src={site.logo}
+                                            alt={site.title}
+                                        />
+                                    ) : (
+                                        <GatsbyImage image={data.file.childImageSharp.gatsbyImageData} alt={site.title} />
                                     )}
-                                    {site.facebook && (
-                                        <a
-                                            href={facebookUrl}
-                                            className="site-nav-item"
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                        >
-                                            <img
-                                                className="site-nav-icon"
-                                                src="/images/icons/facebook.svg"
-                                                alt="Facebook"
-                                            />
-                                        </a>
-                                    )}
+                                </Link>
+                            </div>
+                            <div className="site-mast-right">
+                                {site.twitter && (
                                     <a
+                                        href={twitterUrl}
                                         className="site-nav-item"
-                                        href={`https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/`}
                                         target="_blank"
                                         rel="noopener noreferrer"
                                     >
                                         <img
                                             className="site-nav-icon"
-                                            src="/images/icons/rss.svg"
-                                            alt="RSS Feed"
+                                            src="/images/icons/twitter.svg"
+                                            alt="Twitter"
                                         />
                                     </a>
-                                </div>
-                            </div>
-                            {isHome ? (
-                                <div className="site-banner">
-                                    <h1 className="site-banner-title">
-                                        {site.title}
-                                    </h1>
-                                    <p className="site-banner-desc">
-                                        {site.description}
-                                    </p>
-                                </div>
-                            ) : null}
-                            <nav className="site-nav">
-                                <div className="site-nav-left">
-                                    {/* The navigation items as setup in Ghost */}
-                                    <Navigation
-                                        data={site.navigation}
-                                        navClass="site-nav-item"
-                                    />
-                                </div>
-                                <div className="site-nav-right">
-                                    <Link
-                                        className="site-nav-button"
-                                        to="/about"
+                                )}
+                                {site.facebook && (
+                                    <a
+                                        href={facebookUrl}
+                                        className="site-nav-item"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
                                     >
-                                        About
-                                    </Link>
-                                </div>
-                            </nav>
-                        </div>
-                    </header>
-
-                    <main className="site-main">
-                        {/* All the main content gets inserted here, index.js, post.js */}
-                        {children}
-                    </main>
-                </div>
-
-                <div className="viewport-bottom">
-                    {/* The footer at the very bottom of the screen */}
-                    <footer className="site-foot">
-                        <div className="site-foot-nav container">
-                            <div className="site-foot-nav-left">
-                                <Link to="/">{site.title}</Link> © 2021 &mdash;
-                                Published with{" "}
+                                        <img
+                                            className="site-nav-icon"
+                                            src="/images/icons/facebook.svg"
+                                            alt="Facebook"
+                                        />
+                                    </a>
+                                )}
                                 <a
-                                    className="site-foot-nav-item"
-                                    href="https://ghost.org"
+                                    className="site-nav-item"
+                                    href={`https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/`}
                                     target="_blank"
                                     rel="noopener noreferrer"
                                 >
-                                    Ghost
+                                    <img
+                                        className="site-nav-icon"
+                                        src="/images/icons/rss.svg"
+                                        alt="RSS Feed"
+                                    />
                                 </a>
                             </div>
-                            <div className="site-foot-nav-right">
+                        </div>
+                        {isHome ? (
+                            <div className="site-banner">
+                                <h1 className="site-banner-title">
+                                    {site.title}
+                                </h1>
+                                <p className="site-banner-desc">
+                                    {site.description}
+                                </p>
+                            </div>
+                        ) : null}
+                        <nav className="site-nav">
+                            <div className="site-nav-left">
+                                {/* The navigation items as setup in Ghost */}
                                 <Navigation
                                     data={site.navigation}
-                                    navClass="site-foot-nav-item"
+                                    navClass="site-nav-item"
                                 />
                             </div>
-                        </div>
-                    </footer>
-                </div>
+                            <div className="site-nav-right">
+                                <Link
+                                    className="site-nav-button"
+                                    to="/about"
+                                >
+                                    About
+                                </Link>
+                            </div>
+                        </nav>
+                    </div>
+                </header>
+
+                <main className="site-main">
+                    {/* All the main content gets inserted here, index.js, post.js */}
+                    {children}
+                </main>
             </div>
-        </>
-    );
+
+            <div className="viewport-bottom">
+                {/* The footer at the very bottom of the screen */}
+                <footer className="site-foot">
+                    <div className="site-foot-nav container">
+                        <div className="site-foot-nav-left">
+                            <Link to="/">{site.title}</Link> © 2021 &mdash;
+                            Published with{" "}
+                            <a
+                                className="site-foot-nav-item"
+                                href="https://ghost.org"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Ghost
+                            </a>
+                        </div>
+                        <div className="site-foot-nav-right">
+                            <Navigation
+                                data={site.navigation}
+                                navClass="site-foot-nav-item"
+                            />
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    </>;
 };
 
 DefaultLayout.propTypes = {
@@ -188,24 +180,21 @@ DefaultLayout.propTypes = {
 
 const DefaultLayoutSettingsQuery = (props) => (
     <StaticQuery
-        query={graphql`
-            query GhostSettings {
-                allGhostSettings {
-                    edges {
-                        node {
-                            ...GhostSettingsFields
-                        }
-                    }
-                }
-                file(relativePath: { eq: "ghost-icon.png" }) {
-                    childImageSharp {
-                        fixed(width: 30, height: 30) {
-                            ...GatsbyImageSharpFixed
-                        }
-                    }
-                }
-            }
-        `}
+        query={graphql`query GhostSettings {
+  allGhostSettings {
+    edges {
+      node {
+        ...GhostSettingsFields
+      }
+    }
+  }
+  file(relativePath: {eq: "ghost-icon.png"}) {
+    childImageSharp {
+      gatsbyImageData(width: 30, height: 30, layout: FIXED)
+    }
+  }
+}
+`}
         render={(data) => <DefaultLayout data={data} {...props} />}
     />
 );
