b91a399f88cc653ff80998061bb67cb9fe8c33fc
brandonbrown <brandon@centraflux.com>
2021-09-06 18:49:04 -0400

    init config

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `http://localhost:8000`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://alampintheunderworld.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
