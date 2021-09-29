66cb252ebe52a9b002ac29e2aa4e40e07d28d5f3
Rodney Matambo <rodney@gatsbyjs.com>
2021-09-29 16:09:10 -0400

    update to g4 beta fix env vars

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,9 @@
+const config = require(`../../.ghost.json`).production;
 module.exports = {
-    siteUrl: `http://localhost:8000`, // Site domain. Do not include a trailing slash!
+    siteUrl:
+        process.env.NODE_ENV === `production`
+            ? process.env.SITE_URL || config.siteUrl || `http://localhost:8000`
+            : `http://localhost:8000`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
@@ -13,4 +17,4 @@ module.exports = {
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
-}
+};
