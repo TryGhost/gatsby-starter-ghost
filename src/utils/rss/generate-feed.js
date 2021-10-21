2c7e23f7dc5868ff410b15530ad30c9cdcac6ac0
Rodney Matambo <rodney@gatsbyjs.com>
2021-10-21 17:21:32 -0400

    updating to gatsby 4 stable

--- src/utils/rss/generate-feed.js
+++ src/utils/rss/generate-feed.js
@@ -1,55 +1,69 @@
-const cheerio = require(`cheerio`)
-const tagsHelper = require(`@tryghost/helpers`).tags
-const _ = require(`lodash`)
+const cheerio = require(`cheerio`);
+const tagsHelper = require(`@tryghost/helpers`).tags;
+const _ = require(`lodash`);
 
 const generateItem = function generateItem(siteUrl, post) {
-    const itemUrl = post.canonical_url || `${siteUrl}/${post.slug}/`
-    const html = post.html
-    const htmlContent = cheerio.load(html, { decodeEntities: false, xmlMode: true })
+    const itemUrl = post.canonical_url || `${siteUrl}/${post.slug}/`;
+    const html = post.html;
+    const htmlContent = cheerio.load(html, {
+        decodeEntities: false,
+        xmlMode: true,
+    });
     const item = {
         title: post.title,
         description: post.excerpt,
         guid: post.id,
         url: itemUrl,
         date: post.published_at,
-        categories: _.map(tagsHelper(post, { visibility: `public`, fn: tag => tag }), `name`),
+        categories: _.map(
+            tagsHelper(post, { visibility: `public`, fn: (tag) => tag }),
+            `name`
+        ),
         author: post.primary_author ? post.primary_author.name : null,
         custom_elements: [],
-    }
-    let imageUrl
+    };
+    let imageUrl;
 
     if (post.feature_image) {
-        imageUrl = post.feature_image
+        imageUrl = post.feature_image;
 
         // Add a media content tag
         item.custom_elements.push({
-            'media:content': {
+            "media:content": {
                 _attr: {
                     url: imageUrl,
                     medium: `image`,
                 },
             },
-        })
+        });
 
         // Also add the image to the content, because not all readers support media:content
-        htmlContent(`p`).first().before(`<img src="` + imageUrl + `" />`)
-        htmlContent(`img`).attr(`alt`, post.title)
+        htmlContent(`p`)
+            .first()
+            .before(`<img src="` + imageUrl + `" />`);
+        htmlContent(`img`).attr(`alt`, post.title);
     }
 
     item.custom_elements.push({
-        'content:encoded': {
+        "content:encoded": {
             _cdata: htmlContent.html(),
         },
-    })
-    return item
-}
+    });
+    return item;
+};
 
 const generateRSSFeed = function generateRSSFeed(siteConfig) {
     return {
-        serialize: ({ query: { allGhostPost } }) => allGhostPost.edges.map(edge => Object.assign({}, generateItem(siteConfig.siteUrl, edge.node))),
+        title: `No title`,
+        serialize: ({ query: { allGhostPost } }) =>
+            allGhostPost.edges.map((edge) =>
+                Object.assign({}, generateItem(siteConfig.siteUrl, edge.node))
+            ),
         setup: ({ query: { allGhostSettings } }) => {
-            const siteTitle = allGhostSettings.edges[0].node.title || `No Title`
-            const siteDescription = allGhostSettings.edges[0].node.description || `No Description`
+            const siteTitle =
+                allGhostSettings.edges[0].node.title || `No Title`;
+            const siteDescription =
+                allGhostSettings.edges[0].node.description || `No Description`;
             const feed = {
                 title: siteTitle,
                 description: siteDescription,
@@ -63,10 +77,10 @@ const generateRSSFeed = function generateRSSFeed(siteConfig) {
                     content: `http://purl.org/rss/1.0/modules/content/`,
                     media: `http://search.yahoo.com/mrss/`,
                 },
-            }
+            };
             return {
                 ...feed,
-            }
+            };
         },
         query: `
         {
@@ -116,7 +130,7 @@ const generateRSSFeed = function generateRSSFeed(siteConfig) {
         }
   `,
         output: `/rss`,
-    }
-}
+    };
+};
 
-module.exports = generateRSSFeed
+module.exports = generateRSSFeed;
