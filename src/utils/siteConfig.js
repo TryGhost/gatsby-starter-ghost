970ab78dbd0ff762baaef6fa0e36005cc98fdbcf
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 <christianp1894@gmail.com>
2022-05-10 15:14:22 -0500

    update site metadata

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -3,13 +3,13 @@ module.exports = {
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `Art Of Code`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `Software and Data Science Blog`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `Art Of Code`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
