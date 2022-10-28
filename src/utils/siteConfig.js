39d448968d6c3b280210dd99096affc48a5ff856
AbuThekky <82602169+AbuThekky@users.noreply.github.com>
2022-10-28 22:34:32 +0100

    Update site URL

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -2,8 +2,8 @@ const config = require(`../../.ghost.json`).production;
 module.exports = {
     siteUrl:
         process.env.NODE_ENV === `production`
-            ? process.env.SITE_URL || config.siteUrl || `http://localhost:8000`
-            : `http://localhost:8000`, // Site domain. Do not include a trailing slash!
+            ? process.env.SITE_URL || config.siteUrl || `https://venerable-lolly-3061de.netlify.app`
+            : `https://venerable-lolly-3061de.netlify.app`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
